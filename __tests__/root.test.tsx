b8ed3bbbb417873bc3541d96638fb2a32a27768a
NailMinnigalin <DisappearedCat@yandex.ru>
2025-03-03 06:19:09 +0300

    Add "SignIn button has text 'Sign In'" test

--- __tests__/root.test.tsx
+++ __tests__/root.test.tsx
@@ -1,8 +1,20 @@
 import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
+
 import Page from "../src/app/page";
+import { afterEach } from "vitest";
+
+afterEach(() => cleanup());
 
 test("Root page has signIn button", () => {
   render(<Page />);
-  expect(screen.getByRole("signIn_button")).toBeDefined();
+  expect(screen.getByTestId("signIn_button")).toBeDefined();
+});
+
+test("SignIn button has text 'Sign In'", () => {
+  render(<Page />);
+
+  let signInButton = screen.getByTestId("signIn_button");
+
+  expect(signInButton.textContent).toEqual("Sign In");
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <h1 className="text-3xl font-bold mb-6">Board Game Meetup</h1>
       <div className="space-y-4">
-        <Button role="signIn_button">Click me</Button>
+        <Button data-testid="signIn_button">Sign In</Button>
       </div>
     </div>
   );
