fa44966f59b0be2fa56630659d32de6b009c5564
NailMinnigalin <DisappearedCat@yandex.ru>
2025-03-04 05:34:30 +0300

    add tailwindcss prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-3xl font-bold mb-6">Board Game Meetup</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
+      <h1 className="mb-6 text-3xl font-bold">Board Game Meetup</h1>
       <div className="space-y-4">
         <Button data-testid="signIn_button">Sign In</Button>
       </div>
