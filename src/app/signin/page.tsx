46c640326513b78c9c17373cf1c3236934c080e1
NailMinnigalin <DisappearedCat@yandex.ru>
2025-03-05 12:57:14 +0300

    Create base skeleton of signIn page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,1 +1,20 @@
-export default function SignInPage() {}
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/components/ui/card";
+import { Input } from "@/components/components/ui/input";
+
+export default function SignInPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign In</CardTitle>
+        <CardContent>
+          <form></form>
+        </CardContent>
+      </CardHeader>
+    </Card>
+  );
+}
