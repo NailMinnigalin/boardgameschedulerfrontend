587f8b3f57b01340f823eb54b6665eef49c9b0b3
Nail Minnigalin <minnigalin.ni@finist-soft.ru>
2025-03-26 12:18:15 +0300

    Add "ApiService has signIn method" test

--- __tests__/lib/api-service.test.ts
+++ __tests__/lib/api-service.test.ts
@@ -4,3 +4,8 @@ import { expect, test } from "vitest";
 test.concurrent("ApiService exists", () => {
     expect(ApiService).toBeDefined();
 });
+
+test.concurrent("ApiService has signIn method", () => {
+    let apiService = new ApiService();
+    expect(apiService.signIn).toBeInstanceOf(Function);
+});
